238b6ca02240113ffcb1c4ac126f4c689c2c2289
Philip Gollucci <pgollucci@smgellar.fios-router.home>
2018-06-15 17:13:19 -0400

    sync

--- init.zsh
+++ init.zsh
@@ -3,7 +3,7 @@ p6df::modules::p6svn::deps()    { }
 p6df::modules::p6svn::external::brew() { }
 
 p6df::modules::p6svn::init() {
-    local dir=$P6_DFZ_DATA_PARENT/p6m7g8/p6svn
+    local dir=$P6_DFZ_DATA_DIR/p6m7g8/p6svn
 
     local file
     for file in $dir/lib/*.sh; do
