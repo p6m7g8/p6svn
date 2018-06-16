6526d3ebe0f30cb4120b9d7ff606d1f9006b081b
Philip M. Gollucci <pgollucci@p6m7g8.com>
2018-06-16 17:14:56 -0400

    sync

--- init.zsh
+++ init.zsh
@@ -3,10 +3,15 @@ p6df::modules::p6svn::deps()    { }
 p6df::modules::p6svn::external::brew() { }
 
 p6df::modules::p6svn::init() {
-    local dir=$P6_DFZ_DATA_DIR/p6m7g8/p6svn
 
-    local file
-    for file in $dir/lib/*.sh; do
-	 p6_file_load "$file"
-    done
+  p6_svn_init $P6_DFZ_SRC_DIR/p6m7g8/p6svn
+}
+
+p6_svn_init() {
+  local dir="$1"
+
+  local file
+  for file in $dir/lib/*.sh; do
+    p6_file_load "$file"
+  done
 }
