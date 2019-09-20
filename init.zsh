766d9d9b33425e664dfe736db345116c627fc310
Philip M. Gollucci <pgollucci@p6m7g8.com>
2019-09-20 17:53:48 -0400

    bootstrap

--- init.zsh
+++ init.zsh
@@ -4,14 +4,6 @@ p6df::modules::p6svn::external::brew() { }
 
 p6df::modules::p6svn::init() {
 
-  p6_svn_init $P6_DFZ_SRC_DIR/p6m7g8/p6svn
-}
-
-p6_svn_init() {
-  local dir="$1"
-
-  local file
-  for file in $dir/lib/*.sh; do
-    p6_file_load "$file"
-  done
+  local dir="$P6_DFZ_SRC_DIR/p6m7g8/p6svn"
+  p6_bootstrap "$dir"
 }
