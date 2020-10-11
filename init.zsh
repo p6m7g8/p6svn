07f8058d74ec6cb85a1668f89ccdde069b225f73
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-10-11 16:47:24 -0400

    fix(deps): depends on `p6m7g8/p6common`

--- init.zsh
+++ init.zsh
@@ -1,5 +1,11 @@
 p6df::modules::p6svn::version() { echo "0.0.1" }
-p6df::modules::p6svn::deps()    { }
+
+p6df::modules::p6svn::deps() {
+  ModuleDeps=(
+    p6m7g8/p6common
+  )
+}
+
 p6df::modules::p6svn::external::brew() { }
 
 p6df::modules::p6svn::init() {
